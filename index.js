67c696d37ce272dc83fb1efc839b6446e8ceb983
SadikHasanRafi <sadikkhasan@gmail.com>
2022-05-08 02:45:06 +0600

    product add api added

--- index.js
+++ index.js
@@ -3,35 +3,58 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.port || 5000;
 const app = express();
+
+//using middleware
 app.use(cors());
 app.use(express.json());
 
 //user:admin01
 //password:nUIUD1oXeFhPWbdM
 
-
+//connection with mongodb
 const uri = "mongodb+srv://USER:PASS@cluster0.kg2k3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("inventoryProduct").collection("products");
-  // perform actions on the collection object
-  
-  client.close();
-});
 
+
+
+
+async function mongoServer() {
+    try {
+        await client.connect(err => {
+            const productCollection = client.db("inventoryProduct").collection("products");
+            
+            
+            //add data into database
+            app.post('/addproduct',async (req,res)=>{
+    
+                const newProduct = req.body;
+                console.log(newProduct)
+                const result = await productCollection.insertOne(newProduct)
+                res.send(result)
+            })
+            
+
+
+
+
+
+
+
+
+            
+            client.close();
+          });          
+    } catch (error) {
+        console.error(error)        
+    }
+}
+mongoServer().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server is responding')
   })
 
  
-app.get('/addproduct',(req,res)=>{
-    res.send("add product page")
-})
-
-
-  
-  
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
