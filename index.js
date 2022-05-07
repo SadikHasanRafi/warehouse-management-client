16b6767dbcfaac575afb1e166384a1d07c080462
SadikHasanRafi <sadikkhasan@gmail.com>
2022-05-08 01:23:42 +0600

    env file and mongo server connection complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.port || 5000;
@@ -6,14 +6,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//user:admin01
+//password:nUIUD1oXeFhPWbdM
+
+
+const uri = "mongodb+srv://USER:PASS@cluster0.kg2k3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("inventoryProduct").collection("products");
+  // perform actions on the collection object
+  
+  client.close();
+});
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server is responding')
   })
+
+ 
+app.get('/addproduct',(req,res)=>{
+    res.send("add product page")
+})
+
 
   
   
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
   })
